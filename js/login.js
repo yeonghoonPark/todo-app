adb50c281b16ce660eefb7d725ad3097fb2894eb
yeonghoon Park <huklmnj2@naver.com>
2022-06-26 23:22:50 +0900

    login

---
+++ js/login.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const loginForm = document.querySelector('.login-form');
+const loginInput = document.querySelector('.login-form input:nth-child(1)');
+const loginSubmit = document.querySelector('.login-form input:nth-child(2)');
+console.log(loginSubmit);
